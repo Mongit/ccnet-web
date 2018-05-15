9b16994e0407b0ebf3db30512f406be82d7d5483
Mongit <monserrat.jimenez2@gmail.com>
2018-05-15 12:49:20 -0500

    Stock List Page, no content on it

---
+++ web/web/Pages/Stock/StockController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Configuration;
+using web.Pages.Models;
+
+namespace web.Pages.Stock
+{
+    [Authorize]
+    public class StockController : BaseController
+    {
+        private IConfiguration _config;
+        private IApiProxy Proxy { get; set; }
+
+        public StockController(IConfiguration Configuration, IApiProxy proxy) : base(proxy)
+        {
+            this._config = Configuration;
+            this.Proxy = proxy;
+        }
+    }
+}
---
+++ web/web/Pages/Stock/StockList.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace web.Pages.Stock
+{
+    [Authorize]
+    public class StockListModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
