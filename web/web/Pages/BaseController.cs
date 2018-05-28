528c3409aaa40859f9c60dbff73897a981b840d6
Mongit <monserrat.jimenez2@gmail.com>
2018-05-28 15:10:48 -0500

    Removing debug changes

--- web/web/Pages/BaseController.cs
+++ web/web/Pages/BaseController.cs
@@ -1,15 +1,11 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using web.Pages.Models;
 
 namespace web.Pages
 {
-    [AllowAnonymous]
     public class BaseController: Controller
     {
         private IApiProxy Proxy { get; set; }
@@ -44,28 +40,16 @@ namespace web.Pages
 
         [HttpPost]
         public async Task<IActionResult> ServerCall(ApiProxyModel model)
-        //public async Task<IActionResult> ServerCall()
         {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine(Token);
-            sb.AppendLine(JsonConvert.SerializeObject(model));
 
             try
             {
                 HttpResponseMessage response = await Proxy.ServerCall(model, this.Token);
-                
-                sb.AppendLine(await response.Content.ReadAsStringAsync());
-
-                return Content(sb.ToString());
-                //return Content("Hola inicio");
+                return Content(await response.Content.ReadAsStringAsync());
             }
             catch (Exception ex)
             {
-                sb.AppendLine(ex.Message);
-                sb.AppendLine(ex.StackTrace);
-               
-                return Content(sb.ToString());
-//              throw ex;
+                throw ex;
             }
 
         }
--- web/web/Pages/Cotizaciones/Clientes.cshtml.cs
+++ web/web/Pages/Cotizaciones/Clientes.cshtml.cs
@@ -8,7 +8,7 @@ namespace web.Pages
     {
         public void OnGet()
         {
-            ViewData["Message"] = "Hola Mundo.";
+            
         }
     }
 }
